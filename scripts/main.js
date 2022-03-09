96ec789ac4ed5aacf49e84bf29f88fd78e976b95
trungtin1811 <se.nttin1811@gmail.com>
2022-03-10 01:52:16 +0700

    1

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+}
+
+function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
+    }
+}
